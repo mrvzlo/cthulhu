4ae3789340ea9cd185868ec11206c4c364dd1455
Vladimir Andrejev <andrejevve@gmail.com>
2021-09-03 10:05:49 +0300

    v0.01 fonts

--- js/about.35501129.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),o={class:"about"},c=Object(a["e"])("h1",null,"This is an about page",-1),s=[c];function u(n,t){return Object(a["o"])(),Object(a["d"])("div",o,s)}const b={};b.render=u;t["default"]=b}}]);
-//# sourceMappingURL=about.35501129.js.map
